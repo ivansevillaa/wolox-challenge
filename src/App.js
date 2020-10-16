028566c35795ba37c8cc39d870561b5a6b578d70
Ivan Sevilla <ivansevillaa@gmail.com>
2020-10-16 10:00:35 -0300

    added code-splitting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
-import Home from './pages/Home/index';
-import Login from './pages/Login/index';
-import TechList from './pages/TechList/index';
-import NotFound from './pages/NotFound/index';
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
+const TechList = lazy(() => import('./pages/TechList'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <PrivateRoute exact path="/tech-list">
-          <TechList />
-        </PrivateRoute>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<h1>Cargando...</h1>}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute exact path="/tech-list">
+            <TechList />
+          </PrivateRoute>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 };
